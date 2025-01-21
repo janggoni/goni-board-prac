360045ba53054f36c4f1112654f3d1b061a528a6
janggoni <wkdgudrhs@naver.com>
2025-01-21 15:50:04 +0900

    common add

---
+++ common/outbox-message-relay/src/main/java/goni/board/common/outboxmessagerelay/MessageRelayConfig.java
@@ -0,0 +1,53 @@
+package goni.board.common.outboxmessagerelay;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+// kafka 비동기 처리
+@EnableAsync
+@Configuration
+@ComponentScan("goni.board.common.outboxmessagerelay")
+@EnableScheduling
+public class MessageRelayConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaTemplate<String, String> messageRelayKafkaTemplate() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configProps));
+    }
+    // 트랜젝션 끝날때마다 이벤트 전송을 비동기 처리
+    @Bean
+    public Executor messageRelayPublishEventExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("mr-pub-event-");
+        return executor;
+    }
+    //이벤트 전송이 되지 않은것들
+    @Bean
+    public Executor messageRelayPublishPendingEventExecutor() {
+        return Executors.newSingleThreadScheduledExecutor();
+    }
+}
---
+++ common/outbox-message-relay/src/main/java/goni/board/common/outboxmessagerelay/OutboxEventPublisher.java
@@ -0,0 +1,31 @@
+package goni.board.common.outboxmessagerelay;
+
+import goni.board.common.event.Event;
+import goni.board.common.event.EventPayload;
+import goni.board.common.event.EventType;
+import goni.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxEventPublisher {
+    private final Snowflake outboxIdSnowflake = new Snowflake();
+    private final Snowflake eventIdSnowflake = new Snowflake();
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public void publish(EventType type, EventPayload payload, Long shardKey) {
+        // articleid = 10 , shardKey == articleid
+        // 10 %4 = 물리적 샤드 2
+        Outbox outbox = Outbox.create(
+                outboxIdSnowflake.nextId(),
+                type,
+                Event.of(
+                        eventIdSnowflake.nextId(), type, payload
+                ).toJson(),
+                shardKey % MessageRelayConstants.SHARD_COUNT
+        );
+        applicationEventPublisher.publishEvent(OutboxEvent.of(outbox));
+    }
+}
